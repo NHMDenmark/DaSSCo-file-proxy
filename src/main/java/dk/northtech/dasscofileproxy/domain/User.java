0b911d077356d477e8b1d6590181e40891745d6d
Thomas <Thomas@northtech.dk>
2023-07-02 15:02:59 +0200

    Expanded user mapper

--- src/main/java/dk/northtech/dasscofileproxy/domain/User.java
+++ src/main/java/dk/northtech/dasscofileproxy/domain/User.java
@@ -1,7 +1,11 @@
 package dk.northtech.dasscofileproxy.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
     public String username;
     public String token;
     public String keycloakId;
+    public Set<String> roles = new HashSet<>();
 }
--- src/main/java/dk/northtech/dasscofileproxy/webapi/UserMapper.java
+++ src/main/java/dk/northtech/dasscofileproxy/webapi/UserMapper.java
@@ -1,14 +1,30 @@
 package dk.northtech.dasscofileproxy.webapi;
 
+import dk.northtech.dasscofileproxy.domain.SecurityRoles;
 import dk.northtech.dasscofileproxy.domain.User;
+import jakarta.ws.rs.core.SecurityContext;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Map;
 
 public class UserMapper {
-    public static User from(JwtAuthenticationToken token) {
+    public static User from(SecurityContext securityContext) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) securityContext.getUserPrincipal();
         Map<String, Object> tokenAttributes = token.getTokenAttributes();
         User user = new User();
+        if(securityContext.isUserInRole(SecurityRoles.ADMIN)) {
+            user.roles.add(SecurityRoles.ADMIN);
+        }
+        if(securityContext.isUserInRole(SecurityRoles.USER)) {
+            user.roles.add(SecurityRoles.USER);
+        }
+        if(securityContext.isUserInRole(SecurityRoles.DEVELOPER)) {
+            user.roles.add(SecurityRoles.DEVELOPER);
+        }
+        if(securityContext.isUserInRole(SecurityRoles.SERVICE)) {
+            user.roles.add(SecurityRoles.SERVICE);
+        }
+        System.out.println(securityContext.getClass());
         user.keycloakId = String.valueOf(tokenAttributes.get("sub"));
         user.username = String.valueOf(tokenAttributes.get("preferred_username"));
         user.token = token.getToken().getTokenValue();
