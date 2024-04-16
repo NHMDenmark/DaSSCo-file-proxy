3a2b934998648872139fb0928e3bf4fd3b8d8320
Nicolas <nicolas@northtech.dk>
2024-04-16 14:58:21 +0200

    Added some more annotations to domain classes

--- src/main/java/dk/northtech/dasscofileproxy/domain/UserAccess.java
+++ src/main/java/dk/northtech/dasscofileproxy/domain/UserAccess.java
@@ -1,6 +1,18 @@
 package dk.northtech.dasscofileproxy.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 
-public record UserAccess (Long userAccessId, Long directoryId, String username, String token, Instant creationDatetime) {
+public record UserAccess (
+        @Schema(description = "", example = "")
+        Long userAccessId,
+        @Schema(description = "Id for the directory", example = "")
+        Long directoryId,
+        @Schema(description = "Username for the user", example = "THBO")
+        String username,
+        @Schema(description = "", example = "")
+        String token,
+        @Schema(description = "Date and time of the access", example = "2023-05-24T00:00:00.000Z")
+        Instant creationDatetime) {
 }
