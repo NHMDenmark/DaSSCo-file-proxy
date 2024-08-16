82860572e20cedcbf0a8d678fb85730501e7ba10
Thomas <Thomas@northtech.dk>
2024-08-16 16:09:10 +0200

    Bugfix: Switched before/after, some logging

--- src/main/java/dk/northtech/dasscofileproxy/service/ResourcePool.java
+++ src/main/java/dk/northtech/dasscofileproxy/service/ResourcePool.java
@@ -84,7 +84,10 @@ public abstract class ResourcePool<Resource> {
     }
 
     boolean checkAndAddCreationTime() {
-        if(creationTimes.size() == 3 && creationTimes.getLast().plusSeconds(300).isBefore(Instant.now())) {
+        logger.info("creation times size {}", creationTimes.size());
+        logger.info("last time: {}", creationTimes.size() > 0 ? creationTimes.getLast(): null);
+
+        if(creationTimes.size() == 3 && creationTimes.getLast().plusSeconds(300).isAfter(Instant.now())) {
             return false;
         }
         synchronized (lock) {
