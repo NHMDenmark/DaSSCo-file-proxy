439007cfbf3f6e0a8365ec0dd829a739f7d243a4
Nicolas <nicolas@northtech.dk>
2024-08-30 11:36:00 +0200

    rabbitmq

---
+++ rabbitmq/startup.sh
@@ -0,0 +1,15 @@
+cat /etc/rabbitmq/rabbitmq.conf
+
+echo $KEYCLOAK_SCHEME
+echo $KEYCLOAK_HOSTNAME
+echo $KEYCLOAK_PREFIX
+echo $KEYCLOAK_REALM
+echo $KEYCLOAK_AUD
+sed -i 's/KEYCLOAK_AUD/'"$KEYCLOAK_AUD"'/g' /etc/rabbitmq/rabbitmq.conf
+sed -i 's/KEYCLOAK_SCHEME/'"$KEYCLOAK_SCHEME"'/g' /etc/rabbitmq/rabbitmq.conf
+sed -i 's/KEYCLOAK_HOSTNAME/'"$KEYCLOAK_HOSTNAME"'/g' /etc/rabbitmq/rabbitmq.conf
+sed -i 's/KEYCLOAK_PREFIX/'"$KEYCLOAK_PREFIX"'/g' /etc/rabbitmq/rabbitmq.conf
+sed -i 's/KEYCLOAK_REALM/'"$KEYCLOAK_REALM"'/g' /etc/rabbitmq/rabbitmq.conf
+
+cat /etc/rabbitmq/rabbitmq.conf
+exec rabbitmq-server
